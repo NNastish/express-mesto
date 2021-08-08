9491528354bba4aa46a534f43cbbe132b6505f72
nastish.n <nastish.novikova@yandex.ru>
2021-08-08 23:53:51 +0300

    added console

--- controllers/cards.js
+++ controllers/cards.js
@@ -41,7 +41,6 @@ module.exports.deleteCard = (req, res, next) => {
 };
 
 module.exports.putLike = (req, res, next) => {
-  console.log(req.params);
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
@@ -52,7 +51,6 @@ module.exports.putLike = (req, res, next) => {
 };
 
 module.exports.deleteLike = (req, res, next) => {
-  console.log(req.params);
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
